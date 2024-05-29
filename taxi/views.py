81a2ba76d1ec723b51cee1f5e2e1355b043e0129
Githi54 <chinter.games1@gmail.com>
2024-05-29 18:21:03 +0300

    added manufaturers

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +14,8 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    context_object_name = 'manufacturer_list'
+    paginate_by = 5
