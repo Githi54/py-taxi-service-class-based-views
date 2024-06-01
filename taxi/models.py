b18989e023ac677e025491edd39fc89e62ff14cf
Githi54 <chinter.games1@gmail.com>
2024-06-01 23:03:06 +0300

    fix code style

--- taxi/models.py
+++ taxi/models.py
@@ -6,13 +6,14 @@ from django.urls import reverse
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
-    
+
     class Meta:
         ordering = ("name",)
 
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
+
     def get_absolute_url(self):
         return reverse("taxi:driver-detail", kwargs={"pk": self.id})
 
@@ -23,6 +24,6 @@ class Car(models.Model):
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(Driver, related_name="cars")
-    
+
     def get_absolute_url(self):
         return reverse("taxi:car-detail", kwargs={"pk": self.id})
